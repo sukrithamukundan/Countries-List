46313d66fc354828beee85264576cd7ecc54a6e0
Sukritha KK <sukrithakk5@gmail.com>
2022-06-26 10:51:10 +0530

    Dropdown

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import React from "react";
-const DETAILS_QUERY = `{
-  countries {
+import React, {useState} from 'react';
+import  {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client';
+
+
+// initialize a GraphQL client
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://countries.trevorblades.com'
+});
+
+// write a GraphQL query that asks for names and codes for all countries
+const LIST_COUNTRIES = gql`
+  {
+    countries {
       name
       code
     }
-}`;
+  }
+`;
 
-function App() {
-  const [details, setDetails] = React.useState([]);
-  React.useEffect(() => {
-    fetch('https://countries.trevorblades.com/graphql/', { method: "POST",
-     headers: { "Content-Type": "application/json" }, 
-     body: JSON.stringify({ query: DETAILS_QUERY })
-    }).then(response => response.json())
-    .then(data => setDetails(data.data.countries)) }, []);
+
+  
+// create a component that renders a select input for coutries
+function CountrySelect() {
+  const [country, setCountry] = useState();
+  const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
+
+  if (loading || error) {
+    return <p>{error ? error.message : 'Loading...'}</p>;
+  }
   return (
     <div>
-      <h1>Country List</h1>
-      <ul>
-        {details.map((detail)=>(
-        <li key={detail.id}>{detail.name}</li>))}
-      </ul>
+    <select value={country} onChange={event => setCountry(event.target.value)}>
+      {data.countries.map(country => (
+        <option key={country.code} value={country.code}>
+          {country.name}
+        </option>
+      ))}
+    </select>
+    <h1>{country}</h1>
     </div>
   );
 }
 
-
-
-export default App;
+export default CountrySelect;
